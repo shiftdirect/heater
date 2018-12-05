e2a1a8cf6edf8e9d41b5a2a49d8869bd82c091fa
rljonesau <ray@mrjones.id.au>
2018-12-05 21:48:31 +1100

    keypad pins adjusted to suit final PCB pinout

--- Arduino/BTCDieselHeater/pins.h
+++ Arduino/BTCDieselHeater/pins.h
@@ -39,10 +39,10 @@ const uint8_t OLED_DC_pin = 26;
 const uint8_t OLED_CS_pin = 27;
 
 const uint8_t keyLeft_pin = 25;
-const uint8_t keyRight_pin = 33;
-const uint8_t keyCentre_pin = 32;
+const uint8_t keyRight_pin = 34;
+const uint8_t keyCentre_pin = 33;
 const uint8_t keyUp_pin = 35;
-const uint8_t keyDown_pin = 34;
+const uint8_t keyDown_pin = 32;
 
 const uint8_t ListenOnlyPin = 36;    // input only, no chip pullup
 const uint8_t WiFi_TriggerPin = 39;  // input only, no chip pullup
