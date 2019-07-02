592ace0f0a94167db9ab1e03ea9698c7ebcb33c0
Ray Jones <ray@mrjones.id.au>
2019-07-02 19:02:28 +1000

    and more freertos

--- src/Afterburner/src/Libraries/esp32-owb/owb.c
+++ src/Afterburner/src/Libraries/esp32-owb/owb.c
@@ -33,8 +33,8 @@
 #include <string.h>
 #include <stdlib.h>
 
-#include "FreeRTOS/FreeRTOS.h"
-#include "FreeRTOS/task.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "esp_log.h"
 #include "sdkconfig.h"
 #include "driver/gpio.h"
